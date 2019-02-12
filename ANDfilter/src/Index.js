8252c192d6e8a5f62b64e3e68453e54cdd1e4910
Eunbit Kim <eunbitkim90@gmail.com>
2019-02-12 16:51:24 +0000

    saparated a file into Index.html, Index.js, and mainpage.css

---
+++ ANDfilter/src/Index.js
@@ -0,0 +1,64 @@
+
+  const newCandidates = [
+      { name: "Kerrie", skills: ["JavaScript", "Docker", "Ruby"] },
+      { name: "Mario", skills: ["Python", "AWS"] },
+      { name: "Jacquline", skills: ["JavaScript", "Azure"] },
+      { name: "Kathy", skills: ["JavaScript", "Java"] },
+      { name: "Anna", skills: ["JavaScript", "AWS"] },
+      { name: "Matt", skills: ["PHP", "AWS"] },
+      { name: "Matt", skills: ["PHP", ".Net", "Docker"] },
+    ];
+
+    function removeRowsFromTable(table) {
+      const rows = table.getElementsByTagName("tr");
+
+      while (rows.length > 1) {
+        table.deleteRow(1);
+      }
+    }
+
+    function insertCandidate(tbody, name, skills) {
+      const newRow = tbody.insertRow();
+      const nameCell = newRow.insertCell();
+      const skillCell = newRow.insertCell();
+
+      const candidateName = document.createTextNode(name);
+      const candidateSkills = document.createTextNode(skills.join(', '));
+
+      nameCell.appendChild(candidateName);
+      skillCell.appendChild(candidateSkills);
+    }
+
+    function addCandidatesToTable(table, candidates) {
+      candidates.forEach(candidate => insertCandidate(table, candidate.name, candidate.skills));
+    }
+
+    // added another function
+    function hasSkill(candidate, skill) {
+      return candidate.skills.includes(skill);
+    }
+
+    // function to filter the cadidates by the skills
+    function filterCandidateBySkill(candidates, skill) {
+     const selectedCandidates = [];
+
+     candidates.forEach((candidate) => {
+       if (hasSkill(candidate, skill)) {
+         selectedCandidates.push(candidate);
+       }
+     });
+
+     return selectedCandidates;
+    }
+
+
+    const candidatesTable = document.getElementById("candidates_example");
+    const newCandidatesTable = candidatesTable.cloneNode(true);
+
+    removeRowsFromTable(newCandidatesTable);
+    const newTbody = newCandidatesTable.getElementsByTagName('tbody')[0];
+
+    const filteredCandidates = filterCandidateBySkill(newCandidates, 'JavaScript')
+    addCandidatesToTable(newTbody, filteredCandidates)
+
+    document.body.appendChild(newCandidatesTable);
